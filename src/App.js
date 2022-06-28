18b60df3c41132c20488e69257b328b2db5acd92
ridwan <ridwanediallo99@gmail.com>
2022-06-28 15:13:42 +0000

    Update App.js from functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
 export default App;
