2e85d1709015a023a9a69aa3b79e927acac74aa4
Ridwan <ridwanediallo99@gmail.com>
2022-06-30 20:58:35 +0000

    Add events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,75 +1,166 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
+
 import './Calculator.css';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.onClickHandler = this.onClickHandler.bind(this);
   }
 
+  onClickHandler = (e) => {
+    const values = calculate(this.state, e.target.innerText);
+    this.setState(values);
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
-      <div className="calculator-grid">
+      <div className="container">
         <div className="output">
-          <div className="curr-operand">0</div>
+          <div className="curr-operand">{` ${total || ''} ${operation || ''} ${next || ''}`}</div>
         </div>
-        <button type="button" className="gray-bg">
-          AC
-        </button>
-        <button type="button" className="gray-bg">
-          +/-
-        </button>
-        <button type="button" className="gray-bg">
-          %
-        </button>
-        <button type="button" className="orange-bg">
-          ÷
-        </button>
-        <button type="button" className="gray-bg">
-          7
-        </button>
-        <button type="button" className="gray-bg">
-          8
-        </button>
-        <button type="button" className="gray-bg">
-          9
-        </button>
-        <button type="button" className="orange-bg">
-          x
-        </button>
-        <button type="button" className="gray-bg">
-          4
-        </button>
-        <button type="button" className="gray-bg">
-          5
-        </button>
-        <button type="button" className="gray-bg">
-          6
-        </button>
-        <button type="button" className="orange-bg">
-          -
-        </button>
-        <button type="button" className="gray-bg">
-          1
-        </button>
-        <button type="button" className="gray-bg">
-          2
-        </button>
-        <button type="button" className="gray-bg">
-          3
-        </button>
-        <button type="button" className="orange-bg">
-          +
-        </button>
-        <button type="button" className="gray-bg data-equals span-two">
-          0
-        </button>
-        <button type="button" className="gray-bg">
-          .
-        </button>
-        <button type="button" className="orange-bg">
-          =
-        </button>
+        <div className="keypad">
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            AC
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            +/-
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            %
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="orange-bg"
+          >
+            ÷
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            7
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            8
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            9
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="orange-bg"
+          >
+            x
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            4
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            5
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            6
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="orange-bg"
+          >
+            -
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            1
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            2
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            3
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="orange-bg"
+          >
+            +
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg data-equals span-two"
+          >
+            0
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="gray-bg"
+          >
+            .
+          </button>
+          <button
+            onClick={this.onClickHandler}
+            type="button"
+            className="orange-bg"
+          >
+            =
+          </button>
+        </div>
       </div>
     );
   }
