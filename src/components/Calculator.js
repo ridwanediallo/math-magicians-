ccdc1d9d1b4064d1e05edd47f090696897e70c74
Ridwan <ridwanediallo99@gmail.com>
2022-07-02 00:24:02 +0000

    Refactor with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,169 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
-
 import './Calculator.css';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-    this.onClickHandler = this.onClickHandler.bind(this);
-  }
+const Calculator = () => {
+  const [calculator, setCalculator] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
 
-  onClickHandler = (e) => {
-    const values = calculate(this.state, e.target.innerText);
-    this.setState(values);
+  const onClickHandler = (e) => {
+    const values = calculate(calculator, e.target.innerText);
+    setCalculator(values);
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="container">
+  const { total, next, operation } = calculator;
+  return (
+    <div className="container">
+      <div className="calculator">
         <div className="output">
-          <div className="curr-operand">{` ${total || ''} ${operation || ''} ${next || ''}`}</div>
+          <div>{` ${total || ''} ${operation || ''} ${next || ''}`}</div>
         </div>
         <div className="keypad">
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             AC
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             +/-
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             %
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="orange-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="orange-bg">
             ÷
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             7
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             8
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             9
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="orange-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="orange-bg">
             x
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             4
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             5
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             6
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="orange-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="orange-bg">
             -
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             1
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             2
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             3
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="orange-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="orange-bg">
             +
           </button>
           <button
-            onClick={this.onClickHandler}
+            onClick={onClickHandler}
             type="button"
             className="gray-bg data-equals span-two"
           >
             0
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="gray-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="gray-bg">
             .
           </button>
-          <button
-            onClick={this.onClickHandler}
-            type="button"
-            className="orange-bg"
-          >
+          <button onClick={onClickHandler} type="button" className="orange-bg">
             =
           </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
