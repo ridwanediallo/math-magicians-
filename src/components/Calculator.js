298dab75b8cd27c2315a37300406695f1813b5b6
Ridwan <ridwanediallo99@gmail.com>
2022-07-06 23:51:34 +0000

    Add home page

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
@@ -16,77 +17,154 @@ const Calculator = () => {
 
   const { total, next, operation } = calculator;
   return (
-    <div className="container">
-      <h2>Do some math!</h2>
-      <div className="calculator">
-        <div className="output">
-          <div>{` ${total || ''} ${operation || ''} ${next || ''}`}</div>
+    <Container>
+      <Row>
+        <div className="parent">
+          <h2>Do some math!</h2>
+
+          <div className="calculator">
+            <div className="output">
+              <div>{` ${total || ''} ${operation || ''} ${next || ''}`}</div>
+            </div>
+            <div className="keypad">
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                AC
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                +/-
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                %
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="orange-bg"
+              >
+                ÷
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                7
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                8
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                9
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="orange-bg"
+              >
+                x
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                4
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                5
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                6
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="orange-bg"
+              >
+                -
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                1
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                2
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                3
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="orange-bg"
+              >
+                +
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg data-equals span-two"
+              >
+                0
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="gray-bg"
+              >
+                .
+              </button>
+              <button
+                onClick={onClickHandler}
+                type="button"
+                className="orange-bg"
+              >
+                =
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="keypad">
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            AC
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            +/-
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            %
-          </button>
-          <button onClick={onClickHandler} type="button" className="orange-bg">
-            ÷
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            7
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            8
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            9
-          </button>
-          <button onClick={onClickHandler} type="button" className="orange-bg">
-            x
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            4
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            5
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            6
-          </button>
-          <button onClick={onClickHandler} type="button" className="orange-bg">
-            -
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            1
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            2
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            3
-          </button>
-          <button onClick={onClickHandler} type="button" className="orange-bg">
-            +
-          </button>
-          <button
-            onClick={onClickHandler}
-            type="button"
-            className="gray-bg data-equals span-two"
-          >
-            0
-          </button>
-          <button onClick={onClickHandler} type="button" className="gray-bg">
-            .
-          </button>
-          <button onClick={onClickHandler} type="button" className="orange-bg">
-            =
-          </button>
-        </div>
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="container">
+      <Row>
+        <Col lg={8}>
+          <p className="text-center mt-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+            natus aliquid illum accusamus blanditiis labore quaerat accusantium
+            ab incidunt pariatur commodi explicabo, tenetur nesciunt voluptates
+            quod dolore, sed officia architecto! Laboriosam impedit laudantium,
+            consequatur, tempore ipsum soluta ratione neque, quas explicabo
+            cumque vel iusto?
+          </p>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
